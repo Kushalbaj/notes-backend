85c05050e0cf7de7953d691ce6db7f41268386d4
Kushal Bajracharya <bajrakus20@gmail.com>
2024-03-20 22:14:23 +0545

    added getuser,getuserbyid,deleteuser and updateuser

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -15,4 +15,42 @@ const createUser = async (req, res) => {
     }
   }   
   
-  module.exports = {createUser};
+
+const getUser = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+} 
+
+const getUserbyId = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+ 
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json(user);
+  }
+  catch (err) {
+    res.status(500).json({ message: err.message });
+  } 
+}
+  
+module.exports = {createUser, getUser, getUserbyId,deleteUser,updateUser};
