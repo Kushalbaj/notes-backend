3148b3d9ab7ebc4b28e3028974f044634dc44640
Kushal Bajracharya <bajrakus20@gmail.com>
2024-03-21 20:09:43 +0545

    Add user routes for getting, deleting, and updating user

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {createUser} = require('../controller/user-controller');
+const {createUser,getUser,getUserbyId,deleteUser,updateUser} = require('../controller/user-controller');
 
 // Routes
 router.post('/create-user', createUser);
+router.post('/get-user', getUser);
+router.post('/get-user/:id', getUserbyId);
+router.post('/delete-user/:id', deleteUser);
+router.post('/update-user/:id', updateUser);
 
 module.exports = router;
